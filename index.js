1dc34f17a619757a5738884b1dae4cfbadf1d5f9
whatafuiamdoing <malison.wolfhunt@icloud.com>
2025-03-12 12:10:21 +0100

    scripts

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import RateModule from "./rate.js"
 import ExchangeModule from "./exchange.js"
 const PORT = 5123
 
+import { binanceOrderBook, fetchTraderForSymbol, startExchange } from "./lib/ai.js"
+
 const app = express()
 
 app.use(express.json())
@@ -41,6 +43,55 @@ app.get("/api/rate/arbitrage/exchanges/:code", async (req, res) => {
 
         const arbitrage = exchange.calculateArbitrage(342)
         res.json({ status: 200, message: arbitrage })
+    } catch (err) {
+        log(err)
+    }
+})
+
+// AI prompting
+app.get("/api/ai/:code", async (req, res) => {
+    try {
+        const exchanges = ['binance', 'okx', 'kraken', 'gate'];
+        const symbols = ['BTC/USDT', 'ETH/USDT'];
+        const promises = [];
+        for (let i = 0; i < exchanges.length; i++) {
+            const exchangeName = exchanges[i];
+            promises.push(startExchange(exchangeName, symbols));
+        }
+        const responses = await Promise.all(promises);
+        res.json({ responses })
+    } catch (err) {
+        log(err)
+    }
+})
+
+app.get("/api/order-book/:code", async (req, res) => {
+    try {
+        const exchanges = ['binance', 'okx', 'kraken', 'gate'];
+        const symbols = ['BTC/USDT', 'ETH/USDT'];
+        const promises = [];
+        for (let i = 0; i < exchanges.length; i++) {
+            const exchangeName = exchanges[i];
+            promises.push(binanceOrderBook(exchangeName, symbols));
+        }
+        const responses = await Promise.all(promises);
+        res.json({ responses })
+    } catch (err) {
+        log(err)
+    }
+})
+
+app.get("/api/trade/:code", async (req, res) => {
+    try {
+        const exchanges = ['okx', 'kraken', 'gate'];
+        const symbols = ['BTC/USDT', 'ETH/USDT'];
+        const promises = [];
+        for (let i = 0; i < exchanges.length; i++) {
+            const exchangeName = exchanges[i];
+            promises.push(fetchTraderForSymbol(exchangeName, symbols));
+        }
+        const responses = await Promise.all(promises);
+        res.json({ responses })
     } catch (err) {
         log(err)
     }
