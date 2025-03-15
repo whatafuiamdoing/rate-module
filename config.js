cd92536c580fa36185ffb5dd3fe64f7a3e581bf4
whatafuiamdoing <malison.wolfhunt@icloud.com>
2025-03-15 17:06:09 +0100

    p2p 1.0.2

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export default {
     exchanges: ["binance", "okx", "bybit", "gate", "kraken"],
     fx_url: "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE",
-    alpha_api_key: "" // api key
+    alpha_api_key: "", // api key
+    binance_p2p_url: "https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search"
 }
--- rate.js
+++ rate.js
@@ -2,6 +2,7 @@ import ccxt from "ccxt"
 import config from "./config.js"
 import { computeCustomPrice, fetchMultipleExchange, getSyntheticRate } from "./lib/ticker.js"
 import { fetchFxConversionRates } from "./lib/fx.js"
+import { fetchBinanceP2P, computeOrderMetrics, constructPayload } from "./lib/p2p.js"
 
 function RateConstructor(pair, base, quote, margin) {
     this.syntheticPair = pair
@@ -41,12 +42,40 @@ const cryptoModule = {
 }
 
 const fiatModule = {
-    async fetchRates(symbol) {
+    async fetchRates(symbol, options) {
+        const fxRates = await fetchFxConversionRates(symbol)
 
+        if (arguments.length >= 2) {
+            const payload = constructPayload(options)
 
+            const [buy, sell] = await Promise.all([
+                fetchBinanceP2P({ tradeType: "BUY", ...payload }),
+                fetchBinanceP2P({ tradeType: "SELL", ...payload })
+            ])
 
-        return await fetchFxConversionRates(symbol)
+            let ticker = {}
+            let sellMetrics = computeOrderMetrics(sell)
+            let buyMetrics = computeOrderMetrics(buy)
+            let overallVol = buyMetrics.totalVolume + sellMetrics.totalVolume
+            let overallVWAP = (buyMetrics.vwap * buyMetrics.totalVolume + sellMetrics.vwap * sellMetrics.totalVolume) / overallVol
+
+            ticker = {
+                symbol,
+                bid: buyMetrics.bestPrice,
+                ask: sellMetrics.bestPrice,
+                vwap: overallVWAP,
+                quote: overallVol
+            }
+
+            if (!fxRates)
+                return ticker
+            else
+                return { ticker, fxRates }
+        } else {
+            return fxRates
+        }
     }
+
 }
 
 const RateModule = Object.create(null)
